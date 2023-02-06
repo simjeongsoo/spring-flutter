c3f241c945aacbb6336f110216d02c2b77d3830c
simjeongsoo <simking95@naver.com>
2023-02-06 17:49:09 +0900

    add CrossOrigin

--- flutter-spring/src/main/java/com/sim/flutterspring/FlutterSpringApplication.java
+++ flutter-spring/src/main/java/com/sim/flutterspring/FlutterSpringApplication.java
@@ -2,8 +2,10 @@ package com.sim.flutterspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin
 public class FlutterSpringApplication {
 
     public static void main(String[] args) {
