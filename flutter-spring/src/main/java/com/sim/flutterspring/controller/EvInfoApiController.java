daa8bb24da9a58a171244aeca4fa1fc2aa0348c1
simjeongsoo <simking95@naver.com>
2023-02-07 21:07:22 +0900

    EvInfoApiController - flutter의 request 를 받을 RESTful API Controller

---
+++ flutter-spring/src/main/java/com/sim/flutterspring/controller/EvInfoApiController.java
@@ -0,0 +1,13 @@
+package com.sim.flutterspring.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@RestController("/flutter")
+@RequiredArgsConstructor
+public class EvInfoApiController {
+    //--flutter request를 받을 api--//
+
+
+}
