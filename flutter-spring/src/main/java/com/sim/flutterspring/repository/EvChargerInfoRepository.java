72a1c29ee9667e94c9efd75f16887f0d3d0d0b3a
simjeongsoo <simking95@naver.com>
2023-02-07 16:01:54 +0900

    EvChargerInfoRepository 생성

---
+++ flutter-spring/src/main/java/com/sim/flutterspring/repository/EvChargerInfoRepository.java
@@ -0,0 +1,7 @@
+package com.sim.flutterspring.repository;
+
+import com.sim.flutterspring.entity.EvChargerInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EvChargerInfoRepository extends JpaRepository<EvChargerInfo,Long> {
+}
