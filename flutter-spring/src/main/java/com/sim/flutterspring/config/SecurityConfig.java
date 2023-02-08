4dc871761afa5ec4c474180b3f307643df7b0b3f
simjeongsoo <simking95@naver.com>
2023-02-09 04:58:56 +0900

    SecurityConfig 수정

--- flutter-spring/src/main/java/com/sim/flutterspring/config/SecurityConfig.java
+++ flutter-spring/src/main/java/com/sim/flutterspring/config/SecurityConfig.java
@@ -1,6 +1,11 @@
 package com.sim.flutterspring.config;
 
+import com.sim.flutterspring.jwt.JwtAccessDeniedHandler;
+import com.sim.flutterspring.jwt.JwtAuthenticationEntryPoint;
+import com.sim.flutterspring.jwt.JwtSecurityConfig;
+import com.sim.flutterspring.jwt.TokenProvider;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,18 +14,24 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.CorsFilter;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@Configuration
 public class SecurityConfig {
     private final TokenProvider tokenProvider;
+    private final CorsFilter corsFilter;
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
     public SecurityConfig(TokenProvider tokenProvider,
+                          CorsFilter corsFilter,
                           JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
                           JwtAccessDeniedHandler jwtAccessDeniedHandler) {
         this.tokenProvider = tokenProvider;
+        this.corsFilter = corsFilter;
         this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
         this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
     }
@@ -50,6 +61,8 @@ public class SecurityConfig {
         httpSecurity
                 // token을 사용하는 방식이기 때문에 csrf를 disable
                 .csrf().disable()
+
+                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
 
                 // Exception을 핸들링 할때 만들어둔 클래스 추가
                 .exceptionHandling()
