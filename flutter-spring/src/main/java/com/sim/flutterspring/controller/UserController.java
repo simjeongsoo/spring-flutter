df73eff0381c88b0b458c50a69e44ba166d8fa3f
simjeongsoo <simking95@naver.com>
2023-02-09 03:17:21 +0900

    add CrossOrigin

--- flutter-spring/src/main/java/com/sim/flutterspring/controller/UserController.java
+++ flutter-spring/src/main/java/com/sim/flutterspring/controller/UserController.java
@@ -5,11 +5,12 @@ import com.sim.flutterspring.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-//@CrossOrigin
+@CrossOrigin
 @RestController
 public class UserController {
 
