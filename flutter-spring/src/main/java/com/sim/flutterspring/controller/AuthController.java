57ee855123d14ae6c0c482a30ad7c17ece882e80
simjeongsoo <simking95@naver.com>
2023-02-09 05:35:15 +0900

    AuthController 클래스 생성 - 회원가입 api, 유저 조회 api

---
+++ flutter-spring/src/main/java/com/sim/flutterspring/controller/AuthController.java
@@ -0,0 +1,65 @@
+package com.sim.flutterspring.controller;
+
+import com.sim.flutterspring.jwt.JwtFilter;
+import com.sim.flutterspring.jwt.TokenProvider;
+import com.sim.flutterspring.model.LoginDto;
+import com.sim.flutterspring.model.TokenDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+
+@RestController
+@RequestMapping("/api")
+public class AuthController {
+    //--로그인 API--//
+    private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
+    private final TokenProvider tokenProvider;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+
+    public AuthController(TokenProvider tokenProvider, AuthenticationManagerBuilder authenticationManagerBuilder) {
+        this.tokenProvider = tokenProvider;
+        this.authenticationManagerBuilder = authenticationManagerBuilder;
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<TokenDto> authorize(@Valid @RequestBody LoginDto loginDto) {
+        // LoginDto의 username, password를 파라미터로 받고
+
+        // 이를 이용해 UsernamePasswordAuthenticationToken 을 생성
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword());
+
+        // authenticationToken 을 이용해서 Authentication 객체를 생성하기위해 authenticate() 메서드가 실행될때
+        // CustomUserDetailsService 클래스의 loadUserByUsername() 메서드가 실행
+        // 실행된 결과값을 가지고 Authentication 객체 생성
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        logger.debug("authenticationToken 생성 값 : {}", authenticationToken);
+
+        // authentication 객체를 SecurityContext에 저장하고
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+
+
+        String jwt = tokenProvider.createToken(authentication);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        // JWT Token 을 Response Header 에도 넣어주고
+        httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
+
+        // TokenDto를 이용해서 ResponseBody에도 넣어서 리턴
+        return new ResponseEntity<>(new TokenDto(jwt), httpHeaders, HttpStatus.OK);
+    }
+}
