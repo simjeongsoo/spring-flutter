e9ebac4720d122a29506feee9aa3f147cdad6a24
simjeongsoo <simking95@naver.com>
2023-02-09 04:17:00 +0900

    TokenProvider 생성

---
+++ flutter-spring/src/main/java/com/sim/flutterspring/jwt/TokenProvider.java
@@ -0,0 +1,109 @@
+package com.sim.flutterspring.jwt;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+@Component  // Bean 생성
+public class TokenProvider implements InitializingBean {
+    //--토큰의 생성, 토큰의 유효성 검증을 담당할 Token Provider--//
+
+    private final Logger logger = LoggerFactory.getLogger(TokenProvider.class);
+
+    private static final String AUTHORITIES_KEY = "auth";
+
+    private final String secret;
+    private final long tokenValidityInMilliseconds;
+
+    private Key key;
+
+    public TokenProvider(       // 의존성 주입
+                                @Value("${jwt.secret}") String secret,  // application.yml 설정값 호출
+                                @Value("${jwt.token-validity-in-seconds}") long tokenValidityInSeconds) {
+        this.secret = secret;
+        this.tokenValidityInMilliseconds = tokenValidityInSeconds * 1000;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        //--InitializingBean의 afterPropertiesSet()를 오버라이드--//
+        // secret 값을 Base64 Decode 해서 key변수에 할당
+        byte[] KeyBytes = Decoders.BASE64.decode(secret);
+        // HMAC-SHA algorithms based on the specified key byte array
+        this.key = Keys.hmacShaKeyFor(KeyBytes);
+    }
+
+    public String createToken(Authentication authentication) {
+        //--Authentication 객체의 권한 정보를 이용해서 토큰을 생성하는 createToken 메소드--//
+
+        // 권한 정보 가져옴
+        String authorities = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
+        long now = (new Date()).getTime();
+        // application.yml 에서 설정했던 만료시간을 설정
+        Date validity = new Date(now + this.tokenValidityInMilliseconds);
+
+        // jwt token 생성후 리턴
+        return Jwts.builder()
+                .setSubject(authentication.getName())       // 권한 이름 설정
+                .claim(AUTHORITIES_KEY, authorities)        // key-value 형식으로 이루어진 한 쌍의 정보 : Claim
+                .signWith(key, SignatureAlgorithm.HS512)    // 서명 알고리즘 설정
+                .setExpiration(validity)                    // 유효기간 설정
+                .compact();
+    }
+
+    public Authentication getAuthentication(String token) {
+        //--Token 에 담겨있는 정보를 이용해 Authentication 객체를 리턴하는 메소드 --//
+        Claims claims = Jwts
+                .parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)      // token을 이용해 claims 생성
+                .getBody();
+
+        Collection<? extends GrantedAuthority> authorities =
+                Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(","))  // claims 에서 권한정보를 꺼냄
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList());
+
+        User principal = new User(claims.getSubject(), "", authorities); // 권한정보를 이용해 유저 객체 생성
+
+        return new UsernamePasswordAuthenticationToken(principal, token, authorities);  // 최종적으로 Authentication 객체 리턴
+    }
+
+    public boolean validateToken(String token) {
+        //--토큰의 유효성 검증을 수행하는 validateToken 메소드--//
+        try {
+            // 토큰을 파싱해보고 발생하는 익셉션들틀 캐치, 문제가 있으면 false, 정상이면 true
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+            logger.info("잘못된 JWT 서명입니다.");
+        } catch (ExpiredJwtException e) {
+            logger.info("만료된 JWT 토큰입니다.");
+        } catch (UnsupportedJwtException e) {
+            logger.info("지원되지 않는 JWT 토큰입니다.");
+        } catch (IllegalArgumentException e) {
+            logger.info("JWT 토큰이 잘못 되었습니다.");
+        }
+        return false;
+    }
+}
