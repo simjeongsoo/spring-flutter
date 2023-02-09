7f99a9bd0b96e7f587d1df7e4f3ee13ba13b2aaf
simjeongsoo <simking95@naver.com>
2023-02-09 13:54:50 +0900

    서버에서 충전소 데이터 가져온 뒤 마커를 생성하는 로직

--- test_flutter_app/lib/Screens/employeeDrawer.dart
+++ test_flutter_app/lib/Screens/employeeDrawer.dart
@@ -6,6 +6,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
@@ -13,6 +14,7 @@ import 'package:test_flutter_app/Screens/getEmployee.dart';
 import 'package:test_flutter_app/Screens/login.dart';
 import 'package:test_flutter_app/Screens/registerEmployee.dart';
 import 'package:test_flutter_app/Model/EvChargerInfo.dart';
+import 'package:test_flutter_app/Service/token_service.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:test_flutter_app/Service/location_service.dart'; // google map
 import 'package:custom_info_window/custom_info_window.dart';
@@ -33,14 +35,14 @@ class employeeDrawerState extends State<employeeDrawer> {
   void setCurrentLatLng()async{
     currentLatLng = await LocationService().getCurrentLatLng();
   }
-  String direction; // 마커 클릭시 위치
-  String currentLocation; // 현재 위치
+  String direction; // 마커 클릭시 placeId
+  String currentLocation; // Current placeId
   void setCurrentLocation() async {
-    // Future<String> to String
     currentLocation = await LocationService().getCurrentPlaceId();
   }
 
-  // 경로그리기
+  //========================================================================
+  // 경로그리기 ( 한국 미지원 )
   Set<Polyline> _polylines = Set<Polyline>();
   int _polylineIdCounter = 1;
   void _setPolyline(List<PointLatLng> points){
@@ -65,15 +67,18 @@ class employeeDrawerState extends State<employeeDrawer> {
       ),
     );
   }
+  //========================================================================
 
-  // 애플리케이션에서 지도를 이동하기 위한 컨트롤러
+  // google map 컨트롤러
   GoogleMapController _mapController;
 
   // 시작 위치
   final CameraPosition _initialPosition = CameraPosition(
-    target: LatLng(37.496923, 126.985021),
+    target: LatLng(37.424345, 126.883305),
     zoom: 14.4746,
   );
+
+  //========================================================================
 
   // Spring 에서 받아온 데이터
   List<EvChargerInfo> _evChargerInfoList;
@@ -84,30 +89,51 @@ class employeeDrawerState extends State<employeeDrawer> {
     fetchEvChargerInfoData();
   }
 
-  // get location from Spring
+  // 충전소 데이터 request
   Future<void> fetchEvChargerInfoData() async {
-    // String url = "http://localhost:8081/flutter/get/evInfo";
-    String url = "http://10.0.2.2:8081/flutter/get/evInfo";
-    var uri = Uri.parse(url);
-    final response = await http.get(uri);
-    if (response.statusCode == 200) {
-      setState(() {
-        if((jsonDecode(utf8.decode(response.bodyBytes)) as List) == null) {
-          print("No data available");
-        }
 
-        _evChargerInfoList =
-            (jsonDecode(utf8.decode(response.bodyBytes)) as List).map((e) =>
-                EvChargerInfo.fromJson(e)).toList();
-        if(_evChargerInfoList == null){
-          print("No data available");
-          _evChargerInfoList=[];
-        }
-      });
-    } else {
-      throw Exception('Failed to load data');
+    if(TokenService().isAuthenticated() == null) {
+      print('토큰이 유효하지 않음');
+    }else {
+      // String url = "http://localhost:8081/flutter/get/evInfo";
+      String url = "http://10.0.2.2:8081/flutter/get/evInfo";
+
+      // setToken();
+      Future<String> getToken = TokenService().getToken();
+      String token = await getToken;
+
+      print("충전소 데이터 불러올때 토큰 : $token");
+      Map<String, String> headers = {};
+
+      if (token != null) {
+        headers['Content-Type'] = 'application/json; charset=UTF-8';
+        headers['Authorization'] = 'Bearer $token';
+      }
+      var uri = Uri.parse(url);
+      final response = await http.get(uri, headers: headers);
+      if (response.statusCode == 200) {
+        setState(() {
+          if ((jsonDecode(utf8.decode(response.bodyBytes)) as List) == null) {
+            print("No data available");
+          }
+
+          _evChargerInfoList =
+              (jsonDecode(utf8.decode(response.bodyBytes)) as List).map((e) =>
+                  EvChargerInfo.fromJson(e)).toList();
+
+          if (_evChargerInfoList == null) {
+            print("No data available");
+            _evChargerInfoList = [];
+          }
+        });
+      } else if(response.statusCode == 401){
+        throw Exception('401 에러');
+      }else{
+        throw Exception('Failed to load data');
+      }
     }
   }
+  //========================================================================
 
   // 검색한 장소로 지도를 이동하기 위한 컨트롤러
   TextEditingController _searchController = TextEditingController();
@@ -116,7 +142,6 @@ class employeeDrawerState extends State<employeeDrawer> {
     final double lat = place['geometry']['location']['lat'];
     final double lng = place['geometry']['location']['lng'];
 
-    // final GoogleMapController controller = await _controller.future;
     final GoogleMapController controller = await _mapController;
 
     _mapController.animateCamera(
@@ -125,9 +150,12 @@ class employeeDrawerState extends State<employeeDrawer> {
       ),
     );
   }
-  // custom_info_window
+  //========================================================================
+
+  // custom_info_window 구현 예정
   // CustomInfoWindowController _customInfoWindowController = CustomInfoWindowController();
 
+  //========================================================================
 
   /*// 지도 클릭 시 표시할 장소에 대한 마커 목록
   final List<Marker> markers = [];
@@ -144,13 +172,14 @@ class employeeDrawerState extends State<employeeDrawer> {
     });
   }
   // marker end*/
+  //========================================================================
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('EV charging station'),
-        backgroundColor: Color.fromRGBO(233, 65, 82, 1),
+        title: Text('EV Charging Station'),
+        backgroundColor: Color.fromRGBO(60, 65, 64, 1),
       ),
       body: Container(
         height: double.infinity,
@@ -245,14 +274,15 @@ class employeeDrawerState extends State<employeeDrawer> {
               child: Icon(Icons. zoom_out),
             ),
             FloatingActionButton(
-              heroTag: 'admin',
-              // 관리자 계정으로 접속시에 보이는 버튼으로 구현할 예정
-              // 버튼 클릭시 관리자 페이지로 이동
+              heroTag: 'search route',
+
               // onPressed: () async {
               //   // _mapController.animateCamera(CameraUpdate.zoomOut());
               //   // fetchEvInfoData();
               //   // setCurrentLocation();
-              //
+
+              //   google directions api 사용하려고 했으나 한국에서는 지원하지 않음
+
               //   // var directions = await LocationService().getDirections(currentLocation, direction);
               //   // LocationService().getDirections(currentLocation, direction);
               //   // print("현위치아이디 : "+currentLocation + " 목적지아이디 : " + direction);
@@ -265,7 +295,7 @@ class employeeDrawerState extends State<employeeDrawer> {
               onPressed:() async {
                 _launchUrl();
               } ,
-              child: Icon(Icons.adb_outlined),
+              child: Icon(Icons.alt_route),
             ),
           ],
         ),
@@ -275,9 +305,13 @@ class employeeDrawerState extends State<employeeDrawer> {
           padding: EdgeInsets.only(top: minimumPadding, bottom: minimumPadding),
           children: <Widget>[
             DrawerHeader(
-              child: Text('EV charging station'),
+              child: Text('EV charging Info',
+              style: GoogleFonts.kanit(
+                fontSize: 30,
+
+              ),),
               decoration: BoxDecoration(
-                  color: Color.fromRGBO(233, 65, 82, 1)
+                  color: Color.fromRGBO(60, 65, 64, 1),
               ),
             ),
             ListTile(
@@ -296,6 +330,13 @@ class employeeDrawerState extends State<employeeDrawer> {
                     MaterialPageRoute(builder: (context) => getEmployee()));
               },
             ),
+            ListTile(
+              title: Text('test'),
+              onTap: () {
+                // Navigator.push(context,
+                //     MaterialPageRoute(builder: (context) => testScreen()));
+              },
+            ),
             Container(
               alignment: Alignment.bottomCenter,
               height: 400,
@@ -304,6 +345,7 @@ class employeeDrawerState extends State<employeeDrawer> {
               child: ElevatedButton(
                 child: Text('Logout'),
                 onPressed: () {
+                  TokenService().logout(); // 토큰 삭제
                   Navigator.push(context,
                       MaterialPageRoute(builder: (context) => Login()));
                 },
@@ -314,9 +356,6 @@ class employeeDrawerState extends State<employeeDrawer> {
       ),
     );
   }
-  // final Uri _url = Uri.parse('https://www.google.com');
-  // https://www.google.com/maps/dir/lat,lng/lat,lng/data=!3m1!4b1!4m2!4m1!3e3
-
 
   // 현재위치와 마커의 위치 기반으로 경로찾기
   Future<void> _launchUrl() async {
