8e24817948d488906a942f1339bf8439693e5cb5
simjeongsoo <simking95@naver.com>
2023-02-09 14:21:12 +0900

    register screen

--- test_flutter_app/lib/Screens/register.dart
+++ test_flutter_app/lib/Screens/register.dart
@@ -4,6 +4,7 @@ import 'package:test_flutter_app/Model/user.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
+import 'package:test_flutter_app/Service/register_service.dart';
 
 class Register extends StatefulWidget {
   Register({Key key}) : super(key: key);
@@ -13,22 +14,11 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _nicknameController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
-  User user = User("", "");
-  // static String url = "http://localhost:8081/register";
-  static String url = "http://10.0.2.2:8081/register";
-
-  var uri = Uri.parse(url);
-
-  Future save() async {
-    var res = await http.post(uri,
-        headers: {'Content-Type': 'application/json'},
-        body: json.encode({'email': user.email, 'password': user.password}));
-    print(res.body);
-    if (res.body != null) {
-      Navigator.pop(context);
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,7 +32,7 @@ class _RegisterState extends State<Register> {
                   height: 750,
                   width: MediaQuery.of(context).size.width,
                   decoration: BoxDecoration(
-                    color: Color.fromRGBO(233, 65, 82, 1),
+                    color: Color.fromRGBO(60, 65, 64, 1),
                     boxShadow: [
                       BoxShadow(
                           blurRadius: 10,
@@ -74,21 +64,20 @@ class _RegisterState extends State<Register> {
                           alignment: Alignment.topLeft,
                           child: Text(
                             "Email",
-                            style: GoogleFonts.roboto(
-                              // fontWeight: FontWeight.bold,
+                            style: GoogleFonts.sourceCodePro(
                               fontSize: 30,
                               color: Color.fromRGBO(255, 255, 255, 0.8),
                             ),
                           ),
                         ),
                         TextFormField(
-                          controller: TextEditingController(text: user.email),
+                          controller: _emailController,
                           onChanged: (val) {
-                            user.email = val;
+                            print(val);
                           },
                           validator: (value) {
                             if (value.isEmpty) {
-                              return 'Email is Empty';
+                              return '이메일을 입력하세요';
                             }
                             return null;
                           },
@@ -100,18 +89,19 @@ class _RegisterState extends State<Register> {
                                   borderSide: BorderSide.none)),
                         ),
                         Container(
-                          height: 8,
+                          height: 1,
                           color: Color.fromRGBO(255, 255, 255, 0.4),
                         ),
                         SizedBox(
-                          height: 60,
+                          height: 10,
                         ),
                         Align(
                           alignment: Alignment.topLeft,
                           child: Text(
                             "Password",
-                            style: GoogleFonts.roboto(
+                            style: GoogleFonts.sourceCodePro(
                               // fontWeight: FontWeight.bold,
+                              // height: 1,
                               fontSize: 30,
                               color: Color.fromRGBO(255, 255, 255, 0.8),
                             ),
@@ -119,14 +109,54 @@ class _RegisterState extends State<Register> {
                         ),
                         TextFormField(
                           obscureText: true,
-                          controller:
-                          TextEditingController(text: user.password),
+                          controller: _passwordController,
                           onChanged: (val) {
-                            user.password = val;
+                            print(val);
                           },
                           validator: (value) {
                             if (value.isEmpty) {
-                              return 'Email is Empty';
+                              return '비밀번호를 입력하세요';
+                            }
+                            return null;
+                          },
+                          style: TextStyle(
+                              fontSize: 30,
+                              color: Colors.white,
+                          ),
+                          decoration: InputDecoration(
+                              errorStyle: TextStyle(
+                                fontSize: 20,
+                                color: Colors.black,
+                              ),
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide.none)),
+                        ),
+                        Container(
+                          height: 1,
+                          color: Color.fromRGBO(255, 255, 255, 0.4),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Align(
+                          alignment: Alignment.topLeft,
+                          child: Text(
+                            "NickName",
+                            style: GoogleFonts.sourceCodePro(
+                              // fontWeight: FontWeight.bold,
+                              fontSize: 30,
+                              color: Color.fromRGBO(255, 255, 255, 0.8),
+                            ),
+                          ),
+                        ),
+                        TextFormField(
+                          controller: _nicknameController,
+                          onChanged: (val) {
+                            print(val);
+                          },
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return '별칭을 입력하세요';
                             }
                             return null;
                           },
@@ -138,11 +168,11 @@ class _RegisterState extends State<Register> {
                                   borderSide: BorderSide.none)),
                         ),
                         Container(
-                          height: 8,
+                          height: 1,
                           color: Color.fromRGBO(255, 255, 255, 0.4),
                         ),
                         SizedBox(
-                          height: 60,
+                          height: 1,
                         ),
                         Center(
                           child: InkWell(
@@ -150,8 +180,8 @@ class _RegisterState extends State<Register> {
                               Navigator.pop(context);
                             },
                             child: Text(
-                              "Already have Account ?",
-                              style: GoogleFonts.roboto(
+                              "로그인하러 가기",
+                              style: GoogleFonts.sourceCodePro(
                                   fontWeight: FontWeight.bold,
                                   fontSize: 20,
                                   color: Colors.white),
@@ -172,18 +202,21 @@ class _RegisterState extends State<Register> {
                     // color: Color.fromRGBO(233, 65, 82, 1),
                       style: TextButton.styleFrom(
                           // foregroundColor: const Color.fromRGBO(233, 65, 82, 1),
-                          backgroundColor: const Color.fromRGBO(233, 65, 82, 1),
+                          backgroundColor: const Color.fromRGBO(60, 65, 64, 1),
                           shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(50)
                           )
                       ),
                       onPressed: () {
                         if (_formKey.currentState.validate()) {
-                          save();
+                          registerUser(
+                              context,
+                              _emailController.text,
+                              _passwordController.text,
+                              _nicknameController.text
+                          );
                         }
                       },
-                      // shape: RoundedRectangleBorder(
-                      //     borderRadius: BorderRadius.circular(50)),
                       child: Icon(
                         Icons.arrow_forward,
                         color: Colors.white,
