e722ee4da1cda1b7f7b5c90f25406f66cdeae3a5
simjeongsoo <simking95@naver.com>
2023-02-09 13:43:00 +0900

    사용자 권한 정보 response api

---
+++ flutter-spring/src/main/java/com/sim/flutterspring/controller/AuthorityController.java
@@ -0,0 +1,58 @@
+package com.sim.flutterspring.controller;
+
+import com.sim.flutterspring.entity.User;
+import com.sim.flutterspring.jwt.JwtFilter;
+import com.sim.flutterspring.jwt.TokenProvider;
+import com.sim.flutterspring.model.AuthorityDto;
+import com.sim.flutterspring.model.LoginDto;
+import com.sim.flutterspring.repository.AuthorityRepository;
+import com.sim.flutterspring.repository.UserRepository;
+import com.sim.flutterspring.service.CustomUserDetailsService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api")
+public class AuthorityController {
+
+    private final AuthorityRepository authorityRepository;
+    private final UserRepository userRepository;
+    private final CustomUserDetailsService customUserDetailsService;
+    private final TokenProvider tokenProvider;
+    private final JwtFilter jwtFilter;
+
+    public AuthorityController(AuthorityRepository authorityRepository, UserRepository userRepository, CustomUserDetailsService customUserDetailsService, TokenProvider tokenProvider, JwtFilter jwtFilter) {
+        this.authorityRepository = authorityRepository;
+        this.userRepository = userRepository;
+        this.customUserDetailsService = customUserDetailsService;
+        this.tokenProvider = tokenProvider;
+        this.jwtFilter = jwtFilter;
+    }
+
+//    @GetMapping("/authority")
+////    @PreAuthorize("hasAnyRole('USER','ADMIN')")
+//    public AuthorityDto getMyUserInfo(String username) {
+//        User user = userRepository.findOneWithAuthoritiesByUsername(username).orElse(null);
+//
+//        return ResponseEntity.ok(authorityRepository.findById(id).get());
+//    }
+
+    @PostMapping("/userauthenticationcheck")
+    public ResponseEntity<Authentication> signin(@Valid @RequestBody LoginDto loginDto, HttpServletRequest request) {       // 사용자 인증 정보
+//        Authentication authenticationClient = authenticationManagerBuilder.getObject().authenticate(token);
+
+        String token = jwtFilter.resolveToken(request); // client에서 받아온 헤더에 담긴 토큰
+        Authentication authenticationClient = tokenProvider.getAuthentication(token); // 토큰 정보로 사용자 권한 체크
+//        Authentication authenticationServer = SecurityContextHolder.getContext().getAuthentication();
+
+        if (!authenticationClient.isAuthenticated()) {
+            return null;
+        }
+
+        return ResponseEntity.ok(authenticationClient);
+    }
+}
