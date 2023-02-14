406d3952520e06eb726b976a04baf7e06ecab4db
simjeongsoo <simking95@naver.com>
2023-02-14 23:51:08 +0900

    add comment

--- flutter-spring/src/main/java/com/sim/flutterspring/controller/EvInfoApiController.java
+++ flutter-spring/src/main/java/com/sim/flutterspring/controller/EvInfoApiController.java
@@ -1,22 +1,15 @@
 package com.sim.flutterspring.controller;
 
 import com.sim.flutterspring.entity.EvChargerInfo;
-import com.sim.flutterspring.model.EvInfoDataDto;
 import com.sim.flutterspring.repository.EvChargerInfoRepository;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.Disposable;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/flutter")
@@ -31,6 +24,7 @@ public class EvInfoApiController {
      * 전기차 충전소 info api
      * */
     @GetMapping("/get/evInfo")
+//    @PreAuthorize("hasAnyRole('ADMIN')")
     public List<EvChargerInfo> getEvInfoData() {
     //        EvChargerInfo evChargerInfo = evChargerInfoRepository.findById(1L).orElse(null);
         return evChargerInfoRepository.findAll();
